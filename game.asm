85eb96250239ceaa79520b801769aa1ee52a25e6
tevoran <j.pfeiffer93@gmx.de>
2021-08-26 18:47:59 +0200

    test character gets printed

--- game.asm
+++ game.asm
@@ -3,9 +3,15 @@
 org 0x7C00
 start:
 
+mov ah, 0x0E
+mov al, 'L'
+mov bx, 0x0 
+int 0x10
 
+;padding to fill up the bootsector
 times 510 - ($-$$) db 0
 
+;bootsector marker
 dw 0xAA55
 
 ; fill up to make a floppy image
