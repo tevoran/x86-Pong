aaf2990205d6944e6b7ee7ddcf08dae6eec0ef54
tevoran <j.pfeiffer93@gmx.de>
2021-08-26 20:10:58 +0200

    clearing screen to yellow

--- game.asm
+++ game.asm
@@ -1,12 +1,33 @@
+%define YELLOW 0x0E
+%define BLACK 0x00
+
+%macro CLS 0
+	mov di, 0x00
+	mov al, YELLOW
+	mov cx, 0xFA00
+	repe stosb 
+%endmacro  
 [BITS 16]
 
-org 0x7C00
-start:
+[ORG 0x7C00]
+jmp start ;setting code segment
 
-mov ah, 0x0E
-mov al, 'L'
-mov bx, 0x0 
+start:
+mov ax, 0x00
+mov ds, ax; setting data segment to zero
+mov ax, 0xA000
+mov es, ax; setting the extra segment for pixel drawing purposes
+
+;setting 320x200 256 colors graphics mode
+mov ax, 0x0013
 int 0x10
+
+CLS
+
+;drawing shit
+mov al, YELLOW
+mov di, 0x20
+mov byte [di], al
 
 ;padding to fill up the bootsector
 times 510 - ($-$$) db 0
