a441e54461488662423e71c98766abeba153ae98
tevoran <j.pfeiffer93@gmx.de>
2021-09-17 22:27:14 +0200

    removed unnecessary macros

--- macros.asm
+++ macros.asm
@@ -25,52 +25,6 @@
 	repe stosb 
 %endmacro 
 
-;%macro DRAW_PLAYER1 0
-;	xor ax,ax
-;	mov word [i], ax ;reset loop variable
-;	player1_draw_loop:
-;		;calculating framebuffer offset
-;		mov ax, RES_X
-;		mov word bx, [player1_y]
-;		add word bx, [i]
-;		mul bx
-;		add ax, PLAYER1_X
-;		mov di, ax ;writing the framebuffer offset for the actual writing purposes
-;		mov al, YELLOW ;color code within the 256 color palette
-;		mov cx, PLAYER_WIDTH ;number of repitions in x direction
-;		repe stosb ;write the line of player paddle
-;
-;	;incrementing loop counting variable
-;	mov word ax, [i] ;reading loop variable
-;	inc ax ;incrementing loop variable
-;	mov word [i], ax ;writing loop variable
-;	cmp ax, PLAYER_HEIGHT ;check if loop counter is smaller than PLAYER_HEIGHT
-;	jl player1_draw_loop	;jump if less
-;%endmacro
-
-;%macro DRAW_PLAYER2 0
-;	xor ax,ax
-;	mov word [i], ax ;reset loop variable
-;	player2_draw_loop:
-;		;calculating framebuffer offset
-;		mov ax, RES_X
-;		mov word bx, [player2_y]
-;		add word bx, [i]
-;		mul bx
-;		add ax, PLAYER2_X
-;		mov di, ax ;writing the framebuffer offset for the actual writing purposes
-;		mov al, YELLOW ;color code within the 256 color palette
-;		mov cx, PLAYER_WIDTH ;number of repitions in x direction
-;		repe stosb ;write the line of player paddle
-;
-;	;incrementing loop counting variable
-;	mov word ax, [i] ;reading loop variable
-;	inc ax ;incrementing loop variable
-;	mov word [i], ax ;writing loop variable
-;	cmp ax, PLAYER_HEIGHT ;check if loop counter is smaller than PLAYER_HEIGHT
-;	jl player2_draw_loop	;jump if less
-;%endmacro
-
 %macro DRAW_BALL 0
 	xor ax,ax
 	mov word [i], ax
